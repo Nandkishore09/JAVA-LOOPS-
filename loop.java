a45078fedf98813537f188818a2fc7fc9a9667dc
Nandkishore Suthar <sutharnandkishor05@gmail.com>
2026-07-29 21:03:24 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,71 @@
+// ---------for loop---------
+
+import java.util.*;
+import java.util.Scanner;
+
+// public class loop{
+//     public static void main(String args[]){
+//         // for(int count =0; count<3; count =count +1){
+//         //     System.out.println("nandkishore");
+//         // }
+//         for (int i=0; i<=10; i++){
+//             System.out.println(i,"");
+//         }
+        
+//     }
+// }
+
+
+// ----------while loop---------
+
+// public class loop{
+//     public static void main (String args []){
+//         int i = 0;
+//         while(i<=10){
+//             System.out.println(i);
+//             i++;
+//         }
+//     }
+// }
+
+// // --------- DO while loop -----------
+// public class loop{
+//     public static void main (String args []){
+//         int i =0;
+//         do{
+//             System.out.println(i);
+//             i++;
+//         }while(i<=10);
+//     }
+// }
+
+// ----------practics question -----------
+// // 1. print the sum of first n natural number 
+
+// public class loop{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+        
+//         int sum =0;
+
+//         for(int i=0; i<=n; i++){
+//             sum=sum+i;
+//         }
+//         System.out.println(sum);
+//     }
+// }
+
+//  2. print the table of a number input by the user 
+
+// public class loop {
+//     public static void main (String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for( int i = 1; i<=10;i++){
+
+//             System.out.println( n*i );
+//         }
+//     }
+// }
+
